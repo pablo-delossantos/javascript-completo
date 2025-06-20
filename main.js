063a86a9e7e3cc17a337056570ddc5046f0f4794
Pablo de los Santos <estudiodedalo@gmail.com>
2025-06-20 19:28:54 -0300

    Feat: reemplazo de valores dinámicos en el mensaje

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Eventos: menú
-// Promer arcgumento, lo que queremos escuchar. Segundo argumento, lo que queremos ejecutar.
+// Menu section
+
 document.querySelector("#open-nav-menu").addEventListener("click", function () {
   document.querySelector("header nav .wrapper").classList.add("nav-open")
 })
@@ -10,35 +10,13 @@ document
     document.querySelector("header nav .wrapper").classList.remove("nav-open")
   })
 
-// Tipo de dato string
-const customer = "Sara"
-let balance = 2000
+// Greeting section
 
-let accountNumber = "I456545"
-accountNumber.length // 7 - Se puede usar como validación
+const greetingText = "Good afternoon!"
+const weatherCondition = "Sunny"
+const userLocation = "Rio de Janeiro"
+let temperature = 35.78
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
 
-accountNumber[0] // N - Validar 
-
-accountNumber.slice(0,2) // Dos primeros caracteres
-
-accountNumber = "PT_456545"
-
-accountNumber.replace = ("_","") // PT456545 - Reemplaza
-
-accountNumber = accountNumber.replace = ("_","") // Actualiza con el replace
-
-// Tipo de dato número
-
-let price = 17.55
-typeof price // 'number'
-
-let discount = 2
-
-let finalPrice = price - discount
-
-finalPrice //15.55
-
-Math.round(price) // 18 - redondea hacia arriba o abajo según el decima
-Math.floor(price) // 17 - redondea hacia abajo
-Math.ceil(price) // 18 - furza redondeo hacia arriba
-price.toFixed(2) // redondea decimal
+document.querySelector("#greeting").innerHTML = greetingText
+document.querySelector("p#weather").innerHTML = weatherText
