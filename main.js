840dd9fc24d17ec1a3b20011c246b02c5b6ef63a
Pablo de los Santos <estudiodedalo@gmail.com>
2025-06-22 13:08:38 -0300

    Feat: function celsius to fahrenheit

--- main.js
+++ main.js
@@ -12,11 +12,22 @@ document
 
 // Greeting section
 
+function celsiusToFahr(temperature) {
+  let fahr = (temperature * 9/5 ) + 32
+  return fahr
+}
+
+celsiusToFahr(25)
+celsiusToFahr(30)
+
 const greetingText = "Good afternoon!"
 const weatherCondition = "Sunny"
 const userLocation = "Rio de Janeiro"
-let temperature = 35.78
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
+let temperature = 25
+
+alert("The temperature outside is " + celsiusToFahr(temperature) + "°F")
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`
 
 document.querySelector("#greeting").innerHTML = greetingText
 document.querySelector("p#weather").innerHTML = weatherText
+
