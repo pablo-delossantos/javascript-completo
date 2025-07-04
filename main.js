10e9bd12fcc4daeea1f29edad9a0fbe2e4ff927f
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-07-04 19:10:55 -0300

    Actualizacion

--- main.js
+++ main.js
@@ -13,6 +13,45 @@ const galleryImages = [
   },
 ]
 
+const products = [
+  {
+    title: "AstroFiction",
+    author: "John Doe",
+    price: 49.9,
+    image: "./assets/products/img6.png",
+  },
+  {
+    title: "Space Odissey",
+    author: "Marie Anne",
+    price: 35,
+    image: "./assets/products/img1.png",
+  },
+  {
+    title: "Doomed City",
+    author: "Jason Cobert",
+    price: 0,
+    image: "./assets/products/img2.png",
+  },
+  {
+    title: "Black Dog",
+    author: "John Doe",
+    price: 85.35,
+    image: "./assets/products/img3.png",
+  },
+  {
+    title: "My Little Robot",
+    author: "Pedro Paulo",
+    price: 0,
+    image: "./assets/products/img5.png",
+  },
+  {
+    title: "Garden Girl",
+    author: "Ankit Patel",
+    price: 45,
+    image: "./assets/products/img4.png",
+  },
+]
+
 // Menu section
 function menuHandler() {
   document
@@ -128,8 +167,38 @@ function galleryHandler() {
   })
 }
 
+// Products section
+
+/* <div class="product-item">
+  <img src="./assets/products/img6.png" alt="AstroFiction" />
+  <div class="product-details">
+    <h3 class="product-title">AstroFiction</h3>
+    <p class="product-author">John Doe</p>
+    <p class="price-title">Price</p>
+    <p class="product-price">$ 49.90</p>
+  </div>
+</div> */
+
+function productsHandlrer() {
+  let productsSection = document.querySelector("product-area")
+
+  products.forEach(function (product, index) {
+    let productElm = document.createElement("div")
+    productElm.classList.add("product-item")
+
+    let productImage = document.createElement("img")
+    productImage.scr = product.image
+    productImage.alt = "Image for" + product.title
+
+    productElm.append(productImage)
+    
+    productSection()
+  })
+}
+
 // Page load
 menuHandler()
 greetingHandler()
 clockHandler()
 galleryHandler()
+productsHandlrer()
